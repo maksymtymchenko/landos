888d37d429332c97c10978d47dcabd404a339c69
maximTymchenko <59209140+maximTymchenko@users.noreply.github.com>
2020-03-16 17:25:21 +0200

    Chart
    Add adaptive chart using ChartJS library

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
 const burger = document.getElementById('burger')
 const headerNav = document.getElementById('headerNav')
 
+//Mobile menu
 burger.addEventListener('click', e => {
   burger.classList.toggle('active')
   headerNav.classList.toggle('active-nav')
+})
+
+// Chart
+let ctx = document.getElementById('myChart').getContext('2d')
+let chart = new Chart(ctx, {
+  type: 'line',
+
+  data: {
+    labels: [
+      'Adobe XD',
+      'After Effects',
+      'Adobe Photoshop',
+      'Adobe Illustrator'
+    ],
+    datasets: [
+      {
+        label: 'Our company top skills',
+        backgroundColor: 'white',
+        borderColor: '#4d4d4d',
+        data: [7, 10, 8, 6, 0]
+      }
+    ]
+  }
 })
