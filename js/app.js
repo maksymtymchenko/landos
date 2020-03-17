60c228c353063ca97d5753e54f63d77cf08b5b17
maximTymchenko <59209140+maximTymchenko@users.noreply.github.com>
2020-03-17 13:02:46 +0200

    Slider
    Add adaptive slider

--- js/app.js
+++ js/app.js
@@ -29,3 +29,25 @@ let chart = new Chart(ctx, {
     ]
   }
 })
+
+// Slide
+
+// let swiper = new Swiper('.swiper-container', {
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev'
+//   }
+// })
+
+let swiper = new Swiper('.swiper-container', {
+  cssMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
