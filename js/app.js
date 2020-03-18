08d4d5bdf07a1a6704940e78cabf1411d2380d2a
maximTymchenko <59209140+maximTymchenko@users.noreply.github.com>
2020-03-18 15:48:04 +0200

    Bugs

--- js/app.js
+++ js/app.js
@@ -1,46 +1,35 @@
-const burger = document.getElementById('burger')
-const headerNav = document.getElementById('headerNav')
-
-//Mobile menu
+const burger = document.getElementById('burger'),
+  headerNav = document.getElementById('headerNav')
 burger.addEventListener('click', e => {
-  burger.classList.toggle('active')
-  headerNav.classList.toggle('active-nav')
+  burger.classList.toggle('active'), headerNav.classList.toggle('active-nav')
 })
-
-// Chart
-let ctx = document.getElementById('myChart').getContext('2d')
-let chart = new Chart(ctx, {
-  type: 'line',
-
-  data: {
-    labels: [
-      'Adobe XD',
-      'After Effects',
-      'Adobe Photoshop',
-      'Adobe Illustrator'
-    ],
-    datasets: [
-      {
-        label: 'Our company top skills',
-        backgroundColor: 'white',
-        borderColor: '#4d4d4d',
-        data: [3, 5, 4, 5, 0]
-      }
-    ]
-  }
-})
-
-// Slide
-
-let swiper = new Swiper('.swiper-container', {
-  cssMode: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  mousewheel: true,
-  keyboard: true
-})
+let ctx = document.getElementById('myChart').getContext('2d'),
+  chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Adobe XD',
+        'After Effects',
+        'Adobe Photoshop',
+        'Adobe Illustrator'
+      ],
+      datasets: [
+        {
+          label: 'Our company top skills',
+          backgroundColor: 'white',
+          borderColor: '#4d4d4d',
+          data: [3, 5, 4, 5, 0]
+        }
+      ]
+    }
+  }),
+  swiper = new Swiper('.swiper-container', {
+    cssMode: !0,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    pagination: { el: '.swiper-pagination' },
+    mousewheel: !0,
+    keyboard: !0
+  })
